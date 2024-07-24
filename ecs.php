9a84b340bc456041aa610c232ed45002c0939f3e
Niek ten Hoopen <niek@nthmedia.nl>
2024-07-24 16:05:17 +0200

    Support Craft v5

--- ecs.php
+++ ecs.php
@@ -3,18 +3,15 @@
 declare(strict_types=1);
 
 use craft\ecs\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 
-return static function(ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
+return function(\Symplify\EasyCodingStandard\Config\ECSConfig $config): void {
+    $config->skip([
+        '*/storage/*',
+    ]);
+    $config->paths([
         __DIR__ . '/src',
         __FILE__,
     ]);
-    $parameters->set(Option::SKIP, [
-        __DIR__ . '/tests/_craft/storage',
-    ]);
 
-    $containerConfigurator->import(SetList::CRAFT_CMS_4);
+    $config->import(SetList::CRAFT_CMS_4);
 };
